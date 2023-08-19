704d131f2e1f2771dd254704d4a821ded16623e6
Nana Agyemang-Duah <nana321.nad@gmail.com>
2023-08-19 14:12:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using EcoPowerPlant.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<EcoPowerContext>(options => options.UseSqlServer(connectionString));
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
