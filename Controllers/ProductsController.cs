4dc2a5f5c33de6b6d6b3d7872246be4cdeda60f7
Nana Agyemang-Duah <nana321.nad@gmail.com>
2023-08-31 16:50:23 +0200

    Push from Development branch
    Final push to main branch from development branch

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EcoPowerLogistics.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EcoPowerLogistics.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly EcoPowerSolutionsContext _context;
+
+        public ProductsController(EcoPowerSolutionsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+          if (_context.Products == null)
+          {
+              return NotFound();
+          }
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(short id)
+        {
+          if (_context.Products == null)
+          {
+              return NotFound();
+          }
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(short id, Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+          if (_context.Products == null)
+          {
+              return Problem("Entity set 'EcoPowerSolutionsContext.Products'  is null.");
+          }
+            _context.Products.Add(product);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductExists(product.ProductId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(short id)
+        {
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(short id)
+        {
+            return (_context.Products?.Any(e => e.ProductId == id)).GetValueOrDefault();
+        }
+
+        // GET: api/Products/Order/{orderId}
+        [HttpGet("Order/{orderId}")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductsByOrder(int orderId)
+        {
+            var orderDetails = await _context.OrderDetails
+                .Where(od => od.OrderId == orderId)
+                .Include(od => od.Product)
+                .ToListAsync();
+
+            if (orderDetails == null || orderDetails.Count == 0)
+            {
+                return NotFound();
+            }
+
+            var products = orderDetails.Select(order => order.Product);
+
+            return Ok(products); 
+        }
+    }
+}
